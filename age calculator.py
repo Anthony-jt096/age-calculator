64bece1698b131bea94f5b020ea05b3951d75054
Anthony-jt096 <at1114083@student.francistuttle.edu>
2024-12-11 10:07:10 -0600

    Add files via upload

---
+++ age calculator.py
@@ -0,0 +1,24 @@
+
+age = int(input("please enter age "))
+if age <= 3:
+    print("you're a toddler")
+elif age <= 3:
+    print("you're a child")
+elif age <= 3:
+    print("you're a child")
+elif age <= 12:
+    print("you're a child")
+elif age <= 13:
+    print ("you're a teenager")
+elif age <= 19:
+    print ("you're a teenager")
+elif age <= 20:
+    print ("you're an adult")
+elif age <= 59:
+    print ("you're an adult")
+elif age <= 60:
+    print ("you're a senior")
+elif age <= 130:
+    print ("you're a senior")
+
+
